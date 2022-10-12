129405d4873fdf61acf1dc5bda0ea934bc921703
Ayako-q <ischenkalex@gmail.com>
2022-10-12 22:56:53 +0300

    updt

--- src/main/java/com/example/project9_part2/Camera.java
+++ src/main/java/com/example/project9_part2/Camera.java
@@ -1,5 +1,73 @@
 package com.example.project9_part2;
 
-public class Camera{
+import java.lang.reflect.Type;
 
+public class Camera {
+    private String brand;
+    private String model;
+    private String type;
+    private int isoMin;
+    private int isoMax;
+
+    //setters:
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setIsoMin(int isoMin) {
+        this.isoMin = isoMin;
+    }
+
+    public void setIsoMax(int isoMax) {
+        this.isoMax = isoMax;
+    }
+
+    //getters:
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getIsoMin() {
+        return isoMin;
+    }
+
+    public int getIsoMax() {
+        return isoMax;
+    }
+
+    public String getIsoRange() {
+        if (type.toLowerCase().equals("film"))
+            return "for this type of camera - iso depends on film you will use";
+        return isoMin + "-" + isoMax;
+    }
+
+    public String messageForType() {
+        if (type.toLowerCase().equals("film"))
+            return "You will have to buy a film before using this camera";
+        return "You will have to charge the battery before using this camera";
+    }
+
+    public void printDescription() {
+        System.out.println("Camera specifications:");
+        System.out.println("Camera name: " + brand + " " + model);
+        System.out.println("Camera type: " + type);
+        System.out.println("ISO sensitivity: " + getIsoRange());
+
+    }
 }
--- src/main/java/com/example/project9_part2/Main.java
+++ src/main/java/com/example/project9_part2/Main.java
@@ -1,2 +1,32 @@
-package com.example.project9_part2;public class Main {
+package com.example.project9_part2;
+
+public class Main {
+    public static void main(String[] args){
+        //creating object 1
+        Camera cam1 = new Camera();
+        cam1.setBrand("Olympus");
+        cam1.setModel("AF-10XB");
+        cam1.setType("Film");
+        //creating object 2
+        Camera cam2 = new Camera();
+        cam2.setBrand("Canon");
+        cam2.setModel("Rebel XS");
+        cam2.setType("Digital");
+        cam2.setIsoMin(100);
+        cam2.setIsoMax(1600);
+        //creating object 3
+        Camera cam3 = new Camera();
+        cam3.setBrand("Leica");
+        cam3.setModel("D-Lux 3");
+        cam3.setType("Digital");
+        cam3.setIsoMin(80);
+        cam3.setIsoMax(1600);
+        //Output full description of every object
+        System.out.println("Camera 1:");
+        cam1.printDescription();
+        System.out.println("\nCamera 2:");
+        cam2.printDescription();
+        System.out.println("\nCamera 3:");
+        cam3.printDescription();
+    }
 }
